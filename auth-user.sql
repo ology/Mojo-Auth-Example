48dd1ec97dd4d5588c2f57773ccc28c53d170217
Gene Boggs <gene@ology.net>
2019-12-23 17:40:31 -0800

    Simplify PRIMARY KEY

--- auth-user.sql
+++ auth-user.sql
@@ -1,11 +1,8 @@
 CREATE TABLE account (
-    id SERIAL,
+    id SERIAL PRIMARY KEY,
     name text NOT NULL,
     password text NOT NULL,
     created timestamp with time zone
 );
 
 ALTER TABLE account OWNER TO gene;
-
-ALTER TABLE ONLY account
-    ADD CONSTRAINT idx_16395_primary PRIMARY KEY (id);
