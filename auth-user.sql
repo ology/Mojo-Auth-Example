f3dae1127ba498220b9f1993df4b3c21a49882a1
Gene Boggs <gene@ology.net>
2019-12-24 00:36:43 -0800

    Add sqlite and mysql variations

--- auth-user.sql
+++ auth-user.sql
@@ -1,7 +1,23 @@
--- This is PostgreSQL syntax
+-- PostgreSQL
 CREATE TABLE account (
     id SERIAL PRIMARY KEY,
-    name text NOT NULL UNIQUE,
-    password text NOT NULL,
-    created timestamptz NOT NULL DEFAULT current_timestamp
+    name TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    created TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+-- SQLite
+-- CREATE TABLE account (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     name TEXT NOT NULL UNIQUE,
+--     password TEXT NOT NULL,
+--     created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+-- );
+
+-- MySQL
+-- CREATE TABLE account (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     name VARCHAR(255) NOT NULL UNIQUE,
+--     password VARCHAR(255) NOT NULL,
+--     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP()
+-- );
