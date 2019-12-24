9c65041f1b32663aaab79bd51e4fa693d876b85d
Gene Boggs <gene@ology.net>
2019-12-24 00:10:41 -0800

    Handle SQLite connection strings

--- lib/AuthHelper/Model/Account.pm
+++ lib/AuthHelper/Model/Account.pm
@@ -19,8 +19,14 @@ sub _schema {
     my $config = $app->plugin('Config');
     my $db = $config->{db};
 
-    my $dsn = sprintf 'dbi:%s:database=%s;host=%s;port=%d',
-        $db->{type}, $db->{name}, $db->{host}, $db->{port};
+    my $dsn;
+    if ( $db->{type} eq 'SQLite' ) {
+        $dsn = sprintf 'dbi:%s:dbname=%s', $db->{type}, $db->{name};
+    }
+    else {
+        $dsn = sprintf 'dbi:%s:database=%s;host=%s;port=%d',
+            $db->{type}, $db->{name}, $db->{host}, $db->{port};
+    }
 
     my $schema = AuthHelper::Schema->connect( $dsn, $db->{user}, $db->{pass} );
 
