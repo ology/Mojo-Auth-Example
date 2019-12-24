a874803393c06f3e81dcce28ab40ad06ae1c5bae
Gene Boggs <gene@ology.net>
2019-12-23 18:30:53 -0800

    Fix unique syntax

--- auth-user.sql
+++ auth-user.sql
@@ -1,7 +1,6 @@
 CREATE TABLE account (
     id SERIAL PRIMARY KEY,
-    name text NOT NULL,
+    name text NOT NULL UNIQUE,
     password text NOT NULL,
-    created timestamptz NOT NULL DEFAULT current_timestamp,
-    UNIQUE(name)
+    created timestamptz NOT NULL DEFAULT current_timestamp
 );
