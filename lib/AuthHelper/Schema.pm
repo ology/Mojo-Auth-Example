3baac7f5d642643bcc4eaeea57bdc1e6857e3a74
Gene Boggs <gene@ology.net>
2019-12-28 08:31:24 -0800

    Remove unwanted comments

--- lib/AuthHelper/Schema.pm
+++ lib/AuthHelper/Schema.pm
@@ -1,9 +1,6 @@
 use utf8;
 package AuthHelper::Schema;
 
-# Created by DBIx::Class::Schema::Loader
-# DO NOT MODIFY THE FIRST PART OF THIS FILE
-
 use strict;
 use warnings;
 
@@ -11,10 +8,4 @@ use base 'DBIx::Class::Schema::Config';
 
 __PACKAGE__->load_namespaces;
 
-
-# Created by DBIx::Class::Schema::Loader v0.07049 @ 2019-12-14 23:39:38
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:c8PmSmaC3Gr/K5FK2DN5pA
-
-
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
